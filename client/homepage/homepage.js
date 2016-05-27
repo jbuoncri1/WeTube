b7c0af01379442ac30a04926dd800b9204e1a50f
Kyle Morehead <krmorehead@gmail.com>
2016-05-27 03:18:47 -0700

    added search with a dropdown menu. worked on some styling

--- client/homepage/homepage.js
+++ client/homepage/homepage.js
@@ -2,4 +2,9 @@ angular.module("homepage", [])
 
 .controller("HomepageController", function ($scope){
   $scope.friends = [{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"},{name: "Bob", lastLocation: "homepage"}]
+  $scope.searchOptions;
+  $scope.changeSelected = function(){
+    $scope.searchOptions = ["in people", "by email", "on youtube", "by youtube url"]
+
+  }
 })
