cdefa7957f5b0bfe9ef566e804344904b7046cf5
Kyle Morehead <krmorehead@gmail.com>
2016-05-27 14:04:07 -0700

    updated dropbox to respond to keys

--- client/homepage/homepage.js
+++ client/homepage/homepage.js
@@ -7,4 +7,38 @@ angular.module("homepage", [])
     $scope.searchOptions = ["in people", "by email", "on youtube", "by youtube url"]
 
   }
+
+  $scope.displayOptions = function(){
+    if($scope.searchQuery.length){
+      $scope.searchOptions = [{name:"in people"}, {name:"by email"}, {name:"on youtube"}, {name:"by youtube url"}]
+    } else {
+      $scope.searchOptions = []
+    }
+  }
+
+  $scope.select = function(index){
+    if($scope.searchOptions[$scope.selected]) {
+      $scope.searchOptions[$scope.selected].selected = false;
+    }
+    $scope.selected = index
+    $scope.searchOptions[index].selected = true;
+  }
+
+  $scope.changeSelected = function(){
+    if($scope.searchOptions){
+      if(event.keyCode === 38) {
+        var newSelect = $scope.selected - 1 || 0
+        if(newSelect < 0){
+          newSelect = 0
+        }
+        $scope.select(newSelect)
+      } else if (event.keyCode === 40) {
+        var newSelect = $scope.selected + 1 || 0
+        if(newSelect >= $scope.searchOptions.length){
+          newSelect = $scope.searchOptions.length - 1
+        }
+        $scope.select(newSelect)
+      }
+    }
+  }
 })
